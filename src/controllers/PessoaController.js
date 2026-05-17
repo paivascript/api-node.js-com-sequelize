edee593bdae43956975919bedb03044a8b90ceef
paivascript <richardpaiva0101@gmail.com>
2026-05-17 12:28:24 -0300

    add crud com sequelize

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,24 @@
+const Controller = require('./Controller.js');
+const PessoaServices = require('../services/PessoaServices.js');
+
+const pessoaServices = new PessoaServices();
+
+class PessoaController extends Controller  {
+  constructor(){
+    super(pessoaServices);
+  }
+
+  async pegaMatriculas(req, res) { 
+    const { estudanteId } = req.params;
+    
+    try {
+      const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+      return res.status(200).json(listaMatriculas);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+}
+
+module.exports = PessoaController;

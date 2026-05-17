b8f226f4dc0be0cecf636f322cc65f059c085243
paivascript <richardpaiva0101@gmail.com>
2026-05-17 20:58:58 -0300

    usando operados, query params, funcoes agregadoras na rota de cursos para funcionalidade contar estudantes de turma

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -7,6 +7,24 @@ class MatriculaController extends Controller {
   constructor() {
     super(matriculaServices);
   }
+
+  async pegaMatriculasPorEstudante(req, res) {
+    const { estudante_id } = req.params;
+
+    try {
+      const ListaMatriculasPorEstudante =
+      await matriculaServices.pegaEContaRegistros({
+        estudante_id: Number(estudante_id),
+        status: 'matriculado'
+      });
+
+      return res.status(200).json(ListaMatriculasPorEstudante);
+
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
+  }
+  
 }
 
 module.exports = MatriculaController;
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -15,9 +15,10 @@ router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
 router.delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
 router.get('/pessoas/:estudante_id/matriculas', (req, res) => pessoaController.pegaMatriculasAtivas(req, res));
 router.get('/pessoas/:estudante_id/matriculas/todos', (req, res) => pessoaController.pegaTodasAsMatriculas(req, res));
+router.get('/pessoas/:estudante_id/matriculas/confirmadas', (req, res) => matriculaController.pegaMatriculasPorEstudante(req, res));
 router.get('/pessoas/:estudante_id/matriculas/:id', (req, res) => matriculaController.pegaUm(req, res));
 router.put('/pessoas/:estudante_id/matriculas/:id', (req, res) => matriculaController.atualiza(req, res));
-router.delete('/pessoas/:estudante_id/matriculas/:id', (req, res) => matriculaController.exclui(req, res));
 router.post('/pessoas/:estudante_id/matriculas', (req, res) => matriculaController.criaNovo(req, res));
+router.delete('/pessoas/:estudante_id/matriculas/:id', (req, res) => matriculaController.exclui(req, res));
 
 module.exports = router;

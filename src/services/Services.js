0c60e0cc9a87146cc756ecf36860c29d200a6f26
paivascript <richardpaiva0101@gmail.com>
2026-05-18 19:24:40 -0300

    add implementando transacoes

--- src/services/Services.js
+++ src/services/Services.js
@@ -30,15 +30,21 @@ class Services {
     return dataSource[this.model].create(dadosDoRegistro);
   }
 
-  async atualizaRegistro(dadosAtualizados, where) {
-    const listadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
-      where: { ...where }
-    });
+
+
+  // Código omitido
+  async atualizaRegistro(dadosAtualizados, where, transacao = {}) {
+    const listadeRegistrosAtualizados = await dataSource[this.model]
+      .update(dadosAtualizados, { 
+        where: { ...where },
+        transaction: transacao
+      });
     if (listadeRegistrosAtualizados[0] === 0) {
       return false;
     }
     return true;
   }
+    
 
   async excluiRegistro(id) {
     return dataSource[this.model].destroy({ where: { id: id } });
